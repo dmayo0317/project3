73523bf8197a5fb6e869cfc710274502792235c9
Celenia Chapa <celeniachapa@Celenias-MBP.attlocal.net>
2021-10-20 20:59:54 -0500

    adding to plot js

--- static/js/plotly.js
+++ static/js/plotly.js
@@ -9,76 +9,105 @@ function buildCharts(sample) {
     d3.json(dataUrl).then((data) => {
         console.log("charts", data)
 
+        var responsive = { responsive: true }
+
         var pricing = data.map(d => d.Price);
         var brands = data.map(d => d.Brand);
         var names = data.map(d => d.Name);
         var stores = data.map(d => d.Store)
         var types = data.map(d => d.Type)
 
-
-
-
         console.log("price & brands", pricing, brands, names)
+
         //   var sample_values = resultArray.map(info => info.amount);
 
         // Build a Bubble Chart
-        var bubbleLayout = {
-            title: "Grocery Pricings",
-            margin: { t: 0 },
-            hovermode: "closest",
-            xaxis: { title: "Brand Names" },
-            margin: { t: 30 }
-        };
-        var bubbleData = [
-            {
-                x: brands,
-                y: pricing,
-                //   text: otu_labels,
-                mode: "markers",
-                marker: {
-                    size: pricing,
-                    color: pricing,
-                    // colorscale: "Earth"
-                }
-            }
-        ];
+        // var bubbleLayout = {
+        //     title: "Grocery Pricings",
+        //     margin: { t: 0 },
+        //     hovermode: "closest",
+        //     xaxis: { title: "Brand Names" },
+        //     margin: { t: 30 }
+        // };
+        // var bubbleData = [
+        //     {
+        //         x: stores,
+        //         y: pricing,
+        //         //   text: otu_labels,
+        //         mode: "markers",
+        //         marker: {
+        //             size: pricing,
+        //             color: pricing,
+        //             // colorscale: "Earth"
+        //         }
+        //     }
+        // ];
 
-        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+        // Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
 
 
 
         ///Bar Chart
-
-        //   var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        function onlyUnique(value, index, self) {
+            return self.indexOf(value) === index;
+          }
+          
         var barData = [
             {
-                y: brands,
+                y: brands.filter(onlyUnique),
                 x: pricing,
                 //   text: otu_labels.slice(0, 10).reverse(),
                 type: "bar",
                 orientation: "h",
+                order: "ascending"
+
             }
         ];
 
+
         var barLayout = {
-            title: "Pricing",
-            margin: { t: 30, l: 150 }
+            title: "Brands Pricing Comparison ",
+            margin: { t: 30, l: 150 },
+            xaxis: {
+                title: 'Price',
+                titlefont: {
+                    family: 'Arial, sans-serif',
+                    size: 18,
+                    color: 'black'
+                }
+            },
+            yaxis: {
+                // title: 'Brands',
+                titlefont: {
+                    family: 'Arial, sans-serif',
+                    size: 18,
+                    color: 'black',
+                    margin: 30, 
+                },
+                title: {
+                    display: true,
+                    // text: "Brands",
+                    padding:{
+                        top: 30,
+                        bottom: 30
+                    }
+
+                }
+            },
         };
 
-        Plotly.newPlot("bar", barData, barLayout);
+        Plotly.newPlot("bar", barData, barLayout, responsive);
         //   buildMetadata(userInfo);
-
-
-
         // Beginning Scattr plot
 
         // walmart
         var trace1 = {
-            x: types.count,
-            y: stores,
+            x: types,
+            y: stores["Walmart"],
             mode: 'markers',
-            type: 'lines',
+            name: 'Walmart',
+            type: 'scatter',
             // transforms: [{
             //     type: 'filter',
             //     target: 'y',
@@ -87,22 +116,33 @@ function buildCharts(sample) {
         };
 
         // wholefoods
-        // var trace2 = {
-        //     x: types,
-        //     y: stores,
-        //     mode: 'lines+markers',
-        //     type: 'scatter'
-        // };
+        var trace2 = {
+            x: types,
+            y: stores["Whole Foods"],
+            mode: 'markers',
+            name: 'Whole Foods',
+            type: 'scatter'
+        };
 
-        var data = [trace1];
+        var layout = {
+            xaxis: {
+            //   range: [ 0.75, 5.25 ]
+            },
+            yaxis: {
+            //   range: [0, 8]
+            },
+            title:'Type Store Comparison'
+          };
 
-        Plotly.newPlot('scatter', data);
+        var data = [trace1, trace2];
+
+        Plotly.newPlot('scatter', data, layout, responsive);
 
     });
 }
 
 //Setting up the selector
-d3.json("/api/v1.0/price_select/").then(function (data) {
+d3.json("/api/v1.0/price_select/ ").then(function (data) {
 
     var dropdown = d3.select('#selDataset')
     data.price.forEach(sample => {
